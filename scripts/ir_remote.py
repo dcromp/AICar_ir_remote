64216c9919c1b0d5bcb0b0376b3d6631c0a44fcf
David Crompton <dcromp@Davids-iMac.local>
2018-06-26 21:24:27 +0200

    bug fix

--- scripts/ir_remote.py
+++ scripts/ir_remote.py
@@ -46,31 +46,31 @@ class InfraRedRemote(object):
                 time.sleep(0.00006)
             if(count < 10):
     			return;
-    		count = 0
+            count = 0
             while GPIO.input(IR) == 1 and count < 80:  #4.5ms
-    			count += 1
-    			time.sleep(0.00006)
-    		idx = 0
-    		cnt = 0
-    		data = [0,0,0,0]
+                count += 1
+                time.sleep(0.00006)
+            idx = 0
+            cnt = 0
+            data = [0,0,0,0]
             for i in range(0,32):
-    			count = 0
+                count = 0
                 while GPIO.input(IR) == 0 and count < 15:    #0.56ms
-    				count += 1
-    				time.sleep(0.00006)
-    			count = 0
+                    count += 1
+                    time.sleep(0.00006)
+                count = 0
                 while GPIO.input(IR) == 1 and count < 40:   #0: 0.56mx
-    				count += 1                               #1: 1.69ms
-    				time.sleep(0.00006)
+                    count += 1                               #1: 1.69ms
+                    time.sleep(0.00006)
                 if count > 7:
-    				data[idx] |= 1<<cnt
+                    data[idx] |= 1<<cnt
                 if cnt == 7:
-    				cnt = 0
-    				idx += 1
+                    cnt = 0
+                    idx += 1
                 else:
-    				cnt += 1
-    		if data[0]+data[1] == 0xFF and data[2]+data[3] == 0xFF:  #check
-    			return data[2]
+                    cnt += 1
+            if data[0]+data[1] == 0xFF and data[2]+data[3] == 0xFF:  #check
+                return data[2]
     		else:
                 pass
 
