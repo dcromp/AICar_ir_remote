9c1ac0dc97662798ce024dc4bf52e444bd95fb91
David Crompton <dcromp@Davids-iMac.local>
2018-06-27 18:34:58 +0200

    update simple move actions

--- scripts/simple_move.py
+++ scripts/simple_move.py
@@ -11,6 +11,20 @@ class SimpleMover(object):
 	def __init__(self):
 		self.Ab = AlphaBot2()
 		rospy.Subscriber('/action', String, self.move_cb)
+		rospy.spin()
+
+	def move_cb(self, msg):
+		action = msg
+		if msg == "foward":
+			self.Ab.forward()
+		if msg == "left":
+			self.Ab.left()
+		if msg == "stop":
+			self.Ab.stop()
+		if msg == "right":
+			self.Ab.right()
+		if msg == "backward":
+			self.Ab.backward()
 
 
 if __name__ == '__main__':
